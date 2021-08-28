7d0b545711559ec2497d97f970c5031291d44cfd
sum529-create <sum529@naver.com>
2021-08-28 13:39:01 +0900

    add github page

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ export default class extends React.Component {
     } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
+    // setState -> state객체에 대한 업데이틀 실행. state가 변경되면 컴포넌트는 리렌더링됨
     this.setState({
       isLoading: false,
       condition: weather[0].main,
@@ -44,10 +45,11 @@ export default class extends React.Component {
       );
     }
   };
-
+  // componentDidMount() 즉시 setState()를 호출함 or render 후에 실행됨
   componentDidMount() {
     this.getLocation();
   }
+  // extends로 받은 React 컴포넌트 | 클래스 컴포넌트에서 반드시 구현돼야 할 유일한 메소드
   render() {
     const { isLoading, temp, condition } = this.state;
     return isLoading ? (
@@ -57,3 +59,13 @@ export default class extends React.Component {
     );
   }
 }
+// render에서 isLoading값이 state에서 true이기 때문에 Loading.js로 넘어가서 실행 된 후
+// componentDidMount()로 넘어가서 getLocation이 실행 사용자의 허락 받고 위치 정보 받아와서
+// getWeather의 api를 통해서 받아오고 각각 setState로 isLoading, temp, condition의 값을 받아온다.
+// isLoading값이 false이니까 Weather.js가 실행 temp과 condition을 props로 넘겨준다.
+// Weather.js에서 WeatherOption에 맞게 각각 설정되고 propsType로 props의 타입이 일치하는 지 확인 후에
+// 화면에 나타나게 된다.
+
+/* react lifeCycle
+constructor -> componentWillMount -> render -> componentDidMount 
+*/
